2ba15d8796c54f89accf9a2ece02d5d033775cea
justin <justin.thiede@visions.ch>
2023-04-25 13:58:46 +0200

    Add namespace examples

---
+++ HelloWorld/Namespaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HelloWorld.Models;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer myComputer = new Computer()
+            {
+                Motherboard = "Z690",
+                HasWifi = true,
+                HasLte = false,
+                ReleaseDate = DateTime.Now,
+                Price = 943m,
+                VideoCard = "RTX 2060"
+            };
+
+            Console.WriteLine(myComputer.Motherboard);
+        }
+    }
+}
