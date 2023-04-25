bcbd7eb43f3242437de79caab640fd0e60e47f3d
justin <justin.thiede@visions.ch>
2023-04-25 07:55:52 +0200

    Add method examples

---
+++ HelloWorld/Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intsToCompress = new int[] { 10, 15, 20 };
+            Console.WriteLine(GetSum(intsToCompress));
+        }
+
+        static private int GetSum(int[] intsToCompress)
+        {
+            int total = 0;
+
+            foreach (int num in intsToCompress)
+            {
+                total += num;
+            }
+
+            return total;
+        }
+    }
+}
