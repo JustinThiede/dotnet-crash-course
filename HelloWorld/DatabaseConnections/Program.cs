6c569ec7827b2175ff2d40a0c57af6f326034ca5
justin <justin.thiede@visions.ch>
2023-04-27 11:07:22 +0200

    Add DB connection dapper examples

---
+++ HelloWorld/DatabaseConnections/Program.cs
@@ -0,0 +1,61 @@
+﻿using DatabaseConnections.Models;
+using DatabaseConnections.Data;
+
+namespace DatabaseConnections
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataContextDapper dapper = new DataContextDapper();
+
+            DateTime now = dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+
+            Console.WriteLine(now);
+
+            Computer myComputer = new Computer()
+            {
+                Motherboard = "Z690",
+                HasWifi = true,
+                HasLte = false,
+                ReleaseDate = DateTime.Now.ToString("yyyy-MM-dd hh:MM:ss"),
+                Price = 943m,
+                VideoCard = "RTX 2060"
+            };
+
+            string sql = @"INSERT INTO TutorialAppSchema.Computer (
+                    Motherboard,
+                    HasWifi,
+                    HasLte,
+                    ReleaseDate,
+                    Price,
+                    VideoCard
+                ) VALUES ('" + myComputer.Motherboard
+                        + "','" + myComputer.HasWifi
+                        + "','" + myComputer.HasLte
+                        + "','" + myComputer.ReleaseDate
+                        + "','" + myComputer.Price
+                        + "','" + myComputer.VideoCard
+                + "')";
+
+            bool result = dapper.ExecuteSql(sql);
+
+            sql = @"
+            SELECT
+                    Computer.Motherboard,
+                    Computer.HasWifi,
+                    Computer.HasLte,
+                    Computer.ReleaseDate,
+                    Computer.Price,
+                    Computer.VideoCard
+            FROM TutorialAppSchema.Computer";
+
+            IEnumerable<Computer> computers = dapper.LoadData<Computer>(sql);
+
+            foreach (Computer singleComputer in computers)
+            {
+                Console.WriteLine(singleComputer.Motherboard);
+            }
+        }
+    }
+}
