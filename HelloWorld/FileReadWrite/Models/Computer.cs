910cb2b9a5e32e12e896032e4e68d83d405cf0fa
justin <justin.thiede@visions.ch>
2023-05-11 13:55:11 +0200

    Add file read write examples

---
+++ HelloWorld/FileReadWrite/Models/Computer.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+
+namespace FileReadWrite.Models
+{
+    public class Computer
+    {
+        // public string _motherboard; This would create a field which goes against C# best practices
+        public int ComputerId { get; set; }
+        public string Motherboard { get; set; } = ""; // This creates a property "Motherboard" that has getter and setter methods and creates a private field _motherboard
+        public int CPUCores { get; set; }
+        public bool HasWifi { get; set; }
+        public bool HasLte { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+        public string VideoCard { get; set; } = ""; // String is not nullable thats why we need to set a default value with = "", alternatively we could create a constructor with the default vals
+    }
+}
---
+++ HelloWorld/FileReadWrite/Program.cs
@@ -0,0 +1,51 @@
+﻿using FileReadWrite.Models;
+using FileReadWrite.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace FileReadWrite
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Computer myComputer = new Computer()
+            {
+                Motherboard = "Z690",
+                HasWifi = true,
+                HasLte = false,
+                ReleaseDate = DateTime.Now,
+                Price = 943m,
+                VideoCard = "RTX 2060"
+            };
+
+            string sql = @"INSERT INTO TutorialAppSchema.Computer (
+                    Motherboard,
+                    CPUCores,
+                    HasWifi,
+                    HasLte,
+                    ReleaseDate,
+                    Price,
+                    VideoCard
+                ) VALUES (
+                    @Motherboard,
+                    @CPUCores,
+                    @HasWifi,
+                    @HasLte,
+                    @ReleaseDate,
+                    @Price,
+                    @VideoCard
+                )";
+
+            File.WriteAllText("log.txt", sql + "\n");
+
+            using StreamWriter openFile = new("log.txt", true);
+
+            openFile.WriteLine(sql);
+            openFile.Close();
+
+            String fileText = File.ReadAllText("log.txt");
+
+            Console.WriteLine(fileText);
+        }
+    }
+}
