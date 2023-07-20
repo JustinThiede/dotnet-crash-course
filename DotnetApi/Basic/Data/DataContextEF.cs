f12f887a54a3708a862a58a3726f8d2d086198e1
Justin Thiede <justin.thiede@ecocoach.com>
2023-07-20 07:58:08 +0200

    Add entityframework

---
+++ DotnetApi/Basic/Data/DataContextEF.cs
@@ -0,0 +1,36 @@
+﻿using Basic.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Basic.Data;
+
+public class DataContextEF : DbContext
+{
+    private readonly IConfiguration _config;
+
+    public DataContextEF(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+    public virtual DbSet<UserSalary> UserSalary { get; set; }
+    public virtual DbSet<UserJobInfo> UserJobInfo { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"),
+                optionsBuilder => optionsBuilder.EnableRetryOnFailure());
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("TutorialAppSchema");
+
+        modelBuilder.Entity<User>().HasKey(u => u.UserId);
+
+        modelBuilder.Entity<UserSalary>().HasKey(u => u.UserId);
+
+        modelBuilder.Entity<UserJobInfo>().HasKey(u => u.UserId);
+    }
+}
